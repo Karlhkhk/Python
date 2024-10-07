d63124c8b0a957ffdfa37d0ddca4f02b9088a644
Karlhkhk <karl@hkhk.edu.ee>
2024-10-07 13:01:39 +0300

    Add files via upload

---
+++ K.py
@@ -0,0 +1,94 @@
+
+import turtle
+#Turtle 3
+turtle.penup()
+turtle.goto(-400, -100)
+turtle.pendown()
+turtle.forward(30)
+turtle.right(60)
+turtle.forward(30)
+turtle.forward(30)
+turtle.right(60)
+turtle.forward(30)
+turtle.forward(30)
+turtle.right(60)
+turtle.forward(30)
+turtle.forward(30)
+turtle.right(60)
+turtle.forward(30)
+turtle.forward(30)
+turtle.right(60)
+turtle.forward(30)
+turtle.forward(30)
+turtle.right(60)
+turtle.forward(30)
+
+#Turtle K
+turtle.penup()
+turtle.goto(-500, -100)
+turtle.pendown()
+turtle.forward(30)
+turtle.left(90)
+turtle.forward(30)
+turtle.right(170)
+turtle.forward(30)
+turtle.left(90)
+turtle.forward(30)
+turtle.left(90)
+turtle.forward(30)
+turtle.left(330)
+turtle.forward(30)
+turtle.left(90)
+turtle.forward(30)
+turtle.left(90)
+turtle.forward(30)
+turtle.right(160)
+turtle.forward(30)
+turtle.left(90)
+turtle.forward(30)
+turtle.left(90)
+turtle.forward(75)
+
+
+#Turtle square
+turtle.penup()
+turtle.home()
+turtle.goto(300, -100)
+turtle.pendown()
+turtle.forward (100)
+turtle.right(90)
+turtle.forward (100)
+turtle.right(90)
+turtle.forward (100)
+turtle.right(90)
+turtle.forward (100)
+turtle.right(90)
+
+#Turtle circle
+turtle.penup()
+turtle.home()
+turtle.goto(200, -100)
+turtle.pendown()
+turtle.circle(50)
+
+
+#Turtle triangle
+turtle.penup()
+turtle.home()
+turtle.goto(-200, -100)
+turtle.pendown()
+turtle.forward(100)
+turtle.left(120)
+turtle.forward(100)
+turtle.left(120)
+turtle.forward(100)
+
+
+
+
+ 
+#Tehtud
+turtle.done()
+
+
+
