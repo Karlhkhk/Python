d2de7735f38a3f38ffcd1c4dd6f99dd8c3e21e69
Karlhkhk <karl@hkhk.edu.ee>
2024-11-01 11:44:01 +0200

    Update yl09.py

--- yl09.py
+++ yl09.py
@@ -2,7 +2,8 @@
 #01.11.2024
 #Karl Kold
 import random
-
+import turtle
+"""""
 #06
 nimed = ['Martin', 'Tõnu', 'Andres', 'Tõnu', 'Andres', 'Andres', 'Andres', 'Tõnu', 'Marko', 'Mari', 'Jüri', 'Liis', 'Marko', 'Piret', 'Anu']
 unikaalsed_nimed = []
@@ -53,27 +54,89 @@ for _ in range(11):
 
     elif tehe=="-":
         print(f"{i}  {tehe} {j} = {i-j}")
+        if vastus == i-j:
+            print("Õige")
+            punktid+=1
+        else:
+            print("Vale")
     elif tehe =="*":
         print(f"{i}  {tehe} {j} = {i*j}")
+        if vastus == i*j:
+            print("Õige")
+            punktid+=1
+        else:
+            print("Vale")
     else:
-        print(f"{i} {tehe} {j} = {round(i/j,2)}")
+        print(f"{i} {tehe} {j} = ")
+        vastus = float(input("Vastus: "))
+        if vastus == [round{i/j,2}]:
+            print("Õige")
+            punktid+=1
+        else:
+            print("Vale")
 
+#11
+"""
+
+for i in range(1,6):
+    print("*"*i,end = " ")
+    print()
+for i in range(1,6):
+    print("*"*(6-i),end = " ")
+    print()
+
+for i in range(1,6):
+    print(" "*(6-i)+"*"*i,end = " ")
+    print()
+
+        
+#12
+even_nums = [2, 4, 6, 8, 10, 12, 14, 16, 18, 20, 22, 24, 26, 28, 30, 3, 32, 34, 36, 38]
+
+sum_even = 0
+for i in even_nums:
+    if i%2 == 0:
+        sum_even=0
+    else:
+        break
+
+    print("Tsükkel jõudis lõppu", sum_even)
+
+#13
+1
+2
+3
+4
+5
+6
+7
+8
+9
+10
+11
+12
+13
+14
+ev_data = [
+['vehicle', 'range', 'price'],
+['Tesla Model Y Long Range', '330', '58990'],
+['Volkswagen ID.4 Pro', '260', '39995'],
+['Ford Mustang Mach-E', '300', '42995'],
+['Audi e-tron GT', '238', '102700'],
+['Nissan Leaf', '149', '27400'],
+['BMW iX xDrive50', '324', '83995'],
+['Polestar 2', '265', '45500'],
+['Kia EV6 Long Range', '310', '47795'],
+['Mercedes-Benz EQS 450+', '350', '102310'],
+['Hyundai Kona Electric', '258', '37400']
+]
+
+keskmine_odo=0
+keskmine_hind=0
+for i in ev_data:
+    for j in i:
+        print(j, end=" ")
+        print(f"{j:>30}", end =" ")
+    print()
     
 
-
-"""
-tehted = ['+','-','*','/']
-for _ in range(11):
-    i = random.randint(1,100)
-    j = random.randint(1,100)
-    tehe = random.choice(tehted)
-
-    if tehe=="+":
-        print(f"{i} {tehe} {j} = {i+j}")
-    elif tehe=="-":
-        print(f"{i} {tehe} {j} = {i-j}")
-    elif tehe=="*":
-        print(f"{i} {tehe} {j} = {i*j}")
-    else:
-        print(f"{i} {tehe} {j} = {round(i/j,2)}")
-"""
