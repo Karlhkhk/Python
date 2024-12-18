690d9350f2bcf6dba5c2e7380ce133f7713c32d3
Karlhkhk <karl@hkhk.edu.ee>
2024-12-18 15:17:27 +0200

    .

--- kodutoo.py
+++ kodutoo.py
@@ -6,16 +6,43 @@
 
 #Ül 10
 
-for i in range (3):
-    print(input("Sisesta kolm kaugushüppe tulemust: "))
+# for i in range (3):
+#     print(input("Sisesta kolm kaugushüppe tulemust: "))
+# tulemused = []
+# tulemused = 
+
+#Ül 11
 
 
 
-# def mahlapakkide_arv (ountekogus):
-#     mahlapakkidearv = int(round (ountekogus * 0.4/3))
-#     return mahlapakkidearv
-# kg = int(input("Õunte kogus: "))
-# print(mahlapakkide_arv(kg))
+#Ül 12
+# Euro eeki
+summa = float(input("Sisesta eurodes summa: ")) #Kasutaja sisestab Eurodes summa
+print(f"{summa} EUR = {summa * 15.6466:.2f} EEK") #Printib välja EEKis
+
+# EEK euri
+summa = float(input("Sisesta EEK summa : ")) #Kasutaja sisestab EEkis summa
+print(f"{summa} EEK = {summa / 15.6466:.2f} EUR") #Printib välja euros
+
+
+
+#Ül 13
+
+arv = int(input("Sisesta arv: ")) #Küsib kasutajalt arvu
+
+if (arv % 2) == 0:#Kontrollib kas arv on paariline või paaritu
+   print("{0} on paariline".format(arv)) #Printib vastuse et on paaritu
+else: #Või siis
+   print("{0} on paaritu".format(arv)) #Paaritu
+
+
+#Ül 14
+
+
+
+
+
+#Ül 15
 
 
 
