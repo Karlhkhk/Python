e7a2acfe17f93ca370b980c88fb233907d743964
Karlhkhk <karl@hkhk.edu.ee>
2024-12-18 14:00:01 +0200

    .

---
+++ kodutoo.py
@@ -0,0 +1,21 @@
+#Karl Kold IT-24
+#18.12.24
+#Ülesanne 10, 11, 12 13, 15, 
+
+
+
+#Ül 10
+
+for i in range (3):
+    print(input("Sisesta kolm kaugushüppe tulemust: "))
+
+
+
+# def mahlapakkide_arv (ountekogus):
+#     mahlapakkidearv = int(round (ountekogus * 0.4/3))
+#     return mahlapakkidearv
+# kg = int(input("Õunte kogus: "))
+# print(mahlapakkide_arv(kg))
+
+
+
