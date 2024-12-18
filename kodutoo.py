dfdf7843edb8788e97cbb736f288d8480573be23
Karlhkhk <karl@hkhk.edu.ee>
2024-12-18 15:31:02 +0200

    .

--- kodutoo.py
+++ kodutoo.py
@@ -16,30 +16,27 @@
 
 
 #Ül 12
-# Euro eeki
-summa = float(input("Sisesta eurodes summa: ")) #Kasutaja sisestab Eurodes summa
-print(f"{summa} EUR = {summa * 15.6466:.2f} EEK") #Printib välja EEKis
+# # Euro eeki
+# summa = float(input("Sisesta eurodes summa: ")) #Kasutaja sisestab Eurodes summa
+# print(f"{summa} EUR = {summa * 15.6466:.2f} EEK") #Printib välja EEKis
 
-# EEK euri
-summa = float(input("Sisesta EEK summa : ")) #Kasutaja sisestab EEkis summa
-print(f"{summa} EEK = {summa / 15.6466:.2f} EUR") #Printib välja euros
+# # EEK euri
+# summa = float(input("Sisesta EEK summa : ")) #Kasutaja sisestab EEkis summa
+# print(f"{summa} EEK = {summa / 15.6466:.2f} EUR") #Printib välja euros
 
 
 
-#Ül 13
+# #Ül 13
 
-arv = int(input("Sisesta arv: ")) #Küsib kasutajalt arvu
+# arv = int(input("Sisesta arv: ")) #Küsib kasutajalt arvu
 
-if (arv % 2) == 0:#Kontrollib kas arv on paariline või paaritu
-   print("{0} on paariline".format(arv)) #Printib vastuse et on paaritu
-else: #Või siis
-   print("{0} on paaritu".format(arv)) #Paaritu
+# if (arv % 2) == 0:#Kontrollib kas arv on paariline või paaritu
+#    print("{0} on paariline".format(arv)) #Printib vastuse et on paaritu
+# else: #Või siis
+#    print("{0} on paaritu".format(arv)) #Paaritu
 
 
 #Ül 14
-
-
-
 
 
 #Ül 15
