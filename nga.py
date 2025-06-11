be44a77f3813d57ca71ca1ade8dd3784ff771ced
Karlhkhk <karl@hkhk.edu.ee>
2025-06-11 15:33:36 +0300

    Update nga.py

--- nga.py
+++ nga.py
@@ -1,5 +1,5 @@
 import turtle
-turtle.speed(10)
+turtle.speed(0)
 for i in range (4):
     turtle.fd(50)
     turtle.lt(90)
@@ -27,9 +27,51 @@ for i in range (4):
 
 turtle.penup()
 turtle.lt(90)
-turtle.forward(400)
+turtle.forward(300)
 turtle.pendown()
-turtle.rt(10)
-turtle.fd(100)
+for i in range(5):
+    turtle.rt(60)
+    turtle.fd(100)
+    turtle.rt(120)
+    turtle.fd(100)
+    turtle.rt(120)
+    turtle.fd(100)
+    turtle.lt(30)
+for i in range(5):
+    turtle.rt(60)
+    turtle.fd(100)
+    turtle.rt(120)
+    turtle.fd(100)
+    turtle.rt(120)
+    turtle.fd(100)
+    turtle.lt(75)
+    turtle.rt(60)
+    turtle.fd(100)
+    turtle.rt(120)
+    turtle.fd(100)
+    turtle.rt(120)
+    turtle.fd(100)
+    turtle.lt(75)
+    
 
-turtle.done
+
+turtle.penup()
+turtle.rt(90)
+turtle.forward(700)
+turtle.pendown()
+
+   
+for i in range(30):
+    turtle.lt(50)
+    turtle.fd(50)
+    turtle.rt(125)
+for i in range(25):
+    turtle.lt(50)
+    turtle.fd(50)
+    turtle.rt(127)
+  
+
+
+
+
+    turtle.done
